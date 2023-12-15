e839831550ec0b4032ddc45117ef33e9ad1663e0
xyk <yakun.auth@gmail.com>
2023-12-15 17:54:13 +0800

    学习页面生命周期,路由跳转等

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -2,16 +2,20 @@ import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
 import window from '@ohos.window';
 
+//相当于一个activity
 export default class EntryAbility extends UIAbility {
   onCreate(want, launchParam) {
+    //相当于activity的onCreate
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
   }
 
   onDestroy() {
+    //相当于activity的onDestroy
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
   }
 
   onWindowStageCreate(windowStage: window.WindowStage) {
+    //相当于activity的onStart
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
 
@@ -25,16 +29,19 @@ export default class EntryAbility extends UIAbility {
   }
 
   onWindowStageDestroy() {
+    //相当于onStop
     // Main window is destroyed, release UI related resources
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageDestroy');
   }
 
   onForeground() {
+    //应用切换到前台,相当于onResume
     // Ability has brought to foreground
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onForeground');
   }
 
   onBackground() {
+    //应用切换到前台,相当于onPause
     // Ability has back to background
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onBackground');
   }
